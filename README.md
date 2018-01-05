9e697522bfdc31474b9e3e98efc303d05dba2afc
Pandawan <pandawanfr@gmail.com>
2018-01-05 12:12:57 -0800

    Added Convert option

---
+++ README.md
@@ -0,0 +1,30 @@
+# CryptoLol
+A messenger bot which gives you information about Crypto Currencies.
+
+## Usage
+
+Price/p: Get the price of a specific coin in USD, BTC, and ETH
+```
+> Price BTC
+
+Price of BTC
+USD: 16409.58
+BTC: 1
+ETH: 17.02
+
+# Can also use filler words
+> Price of LTC
+```
+
+Convert/c: Check the price for a specific amount of coin into another currency
+```
+> Convert 1 ENJ BTC
+
+1 ENJ is 0.00002326 BTC
+
+# Can also use filler words
+> Convert 2 BTC to USD
+```
+
+## Contributing
+Do a PR I guess...
--- build/server.js
+++ build/server.js
@@ -8,6 +8,8 @@ const bot = new bootbot({
 });
 
 bot.hear([/^hello( there)?/i, /^hi( there)?/i, /^hey( there)?/i, /^start/i, /^get\s*started/i], (payload, chat) => {
+	if (data.captured) return;
+
 	// Send a text message followed by another text message that contains a typing indicator
 	chat.say('Hello, human friend!').then(() => {
 		chat.say('I\'m CryptoLol!', {
@@ -20,13 +22,10 @@ bot.hear([/^hello( there)?/i, /^hi( there)?/i, /^hey( there)?/i, /^start/i, /^ge
 	});
 });
 
-bot.hear([/^\s*p(?:rice)?\s*$/i], (payload, chat, data) => {
-	chat.say('Please specify for which currency you want the prices (BTC, ETH...)', {
-		typing: true
-	});
-});
+// p(rice) (of) XXX
+bot.hear([/^\s*p(?:rice)?\s*(?:of)?\s+(\S*)\s*$/i], (payload, chat, data) => {
+	if (data.captured) return;
 
-bot.hear([/^\s*p(?:rice)?\s+(.*)\s*$/i], (payload, chat, data) => {
 	let query = data.match[1].toUpperCase();
 	let to = ['USD', 'BTC', 'ETH'];
 	fetch.convertPrice(query, to).then((response) => {
@@ -58,6 +57,62 @@ bot.hear([/^\s*p(?:rice)?\s+(.*)\s*$/i], (payload, chat, data) => {
 	});
 });
 
+// p(rice) (of)
+bot.hear([/^\s*p(?:rice)?\s*(?:of)?\s*$/i], (payload, chat, data) => {
+	if (data.captured) return;
+
+	chat.say('Please specify for which currency you want the prices (BTC, ETH...)', {
+		typing: true
+	}).then(() => {
+		chat.say('Here is an example: price of BTC', {
+			typing: true
+		});
+	});
+});
+
+// c(onvert) ### XXX (to) YYY
+bot.hear([/^\s*c(?:onvert)?\s+([0-9]+(?:[,.][0-9]*)?)\s+(\S*)\s+(?:to\s+)?\s*(\S*)\s*$/i], (payload, chat, data) => {
+	if (data.captured) return;
+
+	let amount = data.match[1].toUpperCase();
+	let from = data.match[2].toUpperCase();
+	let to = data.match[3].toUpperCase();
+
+	fetch.convertPrice(from, to).then((response) => {
+		// Create an output with every value
+		let output = '';
+		if (to && response[to]) {
+			ouput += `${amount} ${from} is ${response[to] * amount} ${to}`;
+			chat.say(`${output}`, {
+				typing: true
+			});
+		}
+		else {
+			chat.say(`Nothing found.`, {
+				typing: true
+			});
+		}
+	}).catch((error) => {
+		console.log(error);
+		chat.say('Oops, something went wrong...', {
+			typing: true
+		}).then(() => {
+			chat.say(error.message);
+		});
+	});
+});
+
+bot.hear([/^\s*c(?:onvert)?\s*(?:.*)?\s*$/i], (payload, chat, data) => {
+	if (data.captured) return;
+
+	chat.say('Please specify your request like so: convert amount original (to) final', {
+		typing: true
+	}).then(() => {
+		chat.say('Here is an example: convert 1 BTC to USD', {
+			typing: true
+		});
+	})
+});
 
 bot.on('error', (err) => {
 	console.log(err.message);
