9fe87c353775530c0b1db00c3aaecdd0cefb135d
Pandawan <pandawanfr@gmail.com>
2018-01-04 19:09:59 -0800

    Quick Messages

--- build/server.js
+++ build/server.js
@@ -6,12 +6,27 @@ const bot = new bootbot({
 	appSecret: process.env.APP_SECRET
 });
 
+bot.hear([/hello( there)?/i, /hi( there)?/i, /hey( there)?/i], (payload, chat) => {
+	// Send a text message followed by another text message that contains a typing indicator
+	chat.say('Hello, human friend!').then(() => {
+		chat.say('I\'m CryptoLol!', {
+			typing: true
+		}).then(() => {
+			chat.say('I give you important information about popular Crypto Currencies.', {
+				typing: true
+			});
+		});
+	});
+});
+
+
 bot.on('error', (err) => {
 	console.log(err.message);
 });
 
 bot.on('message', (payload, chat) => {
 	const text = payload.message.text;
+	console.log(payload);
 	chat.say(`Echo: ${text}`);
 	console.log(`Sent message: "Echo: ${text}"`)
 });
