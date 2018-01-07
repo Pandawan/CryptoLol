825aff64a4e4fc76d3418d4e8c7c2f67301f81bc
Pandawan <pandawanfr@gmail.com>
2018-01-06 18:57:01 -0800

    Maybe fix config issues

--- build/fire.js
+++ build/fire.js
@@ -6,7 +6,7 @@ function init() {
     // If not yet initialized
     if (admin.apps.length === 0) {
         admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount),
+            credential: admin.credential.cert(JSON.parse(serviceAccount)),
             databaseURL: 'https://crypto-lol-bot.firebaseio.com/'
         });
     }
--- src/fire.ts
+++ src/fire.ts
@@ -2,13 +2,13 @@ import * as admin from 'firebase-admin';
 import {
 	ServiceAccount
 } from 'firebase-admin';
-const serviceAccount = process.env.FIRE_SERVICE_ACCOUNT as string | ServiceAccount;
+const serviceAccount = process.env.FIRE_SERVICE_ACCOUNT as string;
 
 export function init() {
 	// If not yet initialized
 	if (admin.apps.length === 0) {
 		admin.initializeApp({
-			credential: admin.credential.cert(serviceAccount),
+			credential: admin.credential.cert(JSON.parse(serviceAccount)),
 			databaseURL: 'https://crypto-lol-bot.firebaseio.com/'
 		});
 	}
